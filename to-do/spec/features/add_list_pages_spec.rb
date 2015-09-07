10d0b862d3cbc183619b2cd58db6c244ce37e151
David Garber <davidgarber@gmail.com>
2015-09-07 10:19:54 -0700

    describe the add a list process.

--- to-do/spec/features/add_list_pages_spec.rb
+++ to-do/spec/features/add_list_pages_spec.rb
@@ -3,5 +3,9 @@ require 'rails_helper'
 describe "the add a list process" do
   it "adds a new list" do
     visit lists_path
+    click_on 'Add a new list'
+    fill_in 'Name', :with => "School work"
+    click_on 'Create List'
+    expect(page).to have_content 'Lists'
   end
 end
